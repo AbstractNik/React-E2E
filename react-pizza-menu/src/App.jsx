2ef1047090323d48432e06b8b5b8a370fa578963
AbstractNik <nikhiltyagi2000@gmail.com>
2025-11-29 21:36:38 +0000

    components in pizza app

---
+++ react-pizza-menu/src/App.jsx
@@ -0,0 +1,17 @@
+import './App.css'
+import Pizza from './components/Pizza.jsx'
+
+function App() {
+
+
+  return (
+    <>
+    <Pizza />
+      <Pizza />
+      <Pizza />
+      
+    </>
+  )
+}
+
+export default App
---
+++ react-pizza-menu/src/components/Pizza.jsx
@@ -0,0 +1,11 @@
+function Pizza() {
+    return (
+      <>
+        <h2>Spinaci Pizza</h2>
+        <p>Tomato, mozzarella, spinach, and ricotta cheese</p>
+        <img src="../src/pizzas/spinaci.jpg" alt="Spinaci Pizza" />
+      </>
+    );
+  }
+  
+  export default Pizza;
